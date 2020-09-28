476d5b7c97138caf7fcf05efe9669be6023ebf15
TheMagicFelix <riccardo.felisi01@gmail.com>
2020-09-28 12:58:53 +0200

    Add expense summary to dashboard

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { ExpensesSummary } from '../../components/ExpensesSummary';
+
+configure({ adapter: new Adapter() });
+
+test('should correctly render ExpensesSummary with 1 expense', () => {
+	const wrapper = shallow(
+		<ExpensesSummary expenseCount={1} expensesTotal={235} />
+	);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should correctly render ExpensesSummary with multiple expenses', () => {
+	const wrapper = shallow(
+		<ExpensesSummary expenseCount={23} expensesTotal={23512340987} />
+	);
+	expect(wrapper).toMatchSnapshot();
+});
